8d789295dc0dad2b59cda6758620ea5bd88d7c68
luidyzin2007-disign <luidyzin2007@gmail.com>
2025-10-02 08:49:12 -0300

    adicionar conteudo

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function mudar_conteudo_elemento(){
-    document.getElementById(demo).innerHTML="o clique esta funcionando";
+function mudar_texto_elemento(){
+
+    document.getElementById("demo").innerHTML= "o clique esta funcionando";
+
+}
+
+function mudar_cor_elemento(){
+
+    const paragrafo = document.querySelector('#paragrafo');
+    
+    paragrafo.addEventListener('click', () => {
+    
+        const elemento_paragrafo = document.querySelector('#paragrafo');
+    
+        elemento_paragrafo.style.color = 'red';
+})
 }
